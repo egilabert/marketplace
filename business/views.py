9e6d5f1fdfe80f7ea12cd37c2b07e06c7b2c5b1e
Enric Gilabert <gilabert.enric@gmail.com>
2016-11-22 18:07:15 +0100

    auth i home page

--- business/views.py
+++ business/views.py
@@ -8,8 +8,18 @@ import allauth.account.forms as forms
 class LandingView(View):
 	def get(self, request, *args, **kwargs):
 		form = forms.LoginForm()
+		form_signup = forms.SignupForm()
+		print ""
 		print(form)
-		return render(request, "landing.html", {'form': form})
+		print ""
+		return render(request, "landing.html", {'form': form, 'form_signup': form_signup})
+
+	def post(self, request, *args, **kwargs):
+		return HttpResponse('<h1>Home POST page</h1>')
+
+class AppsView(View):
+	def get(self, request, *args, **kwargs):
+		return render(request, "home.html", {})
 
 	def post(self, request, *args, **kwargs):
 		return HttpResponse('<h1>Home POST page</h1>')
--- static/js/init.js
+++ static/js/init.js
@@ -3,7 +3,58 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-    $('.modal').modal();
     
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+$(document).ready(function(){
+
+  $('#modal1').modal({
+    dismissible: true, // Modal can be dismissed by clicking outside of the modal
+    opacity: .5, // Opacity of modal background
+    in_duration: 300, // Transition in duration
+    out_duration: 200, // Transition out duration
+    starting_top: '4%', // Starting top style attribute
+    ending_top: '10%', // Ending top style attribute
+    ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+        $('#modal2').modal('close');
+        $('#modal3').modal('close');
+        console.log(modal, trigger);
+      }
+  }
+  );
+
+  $('#modal2').modal({
+    dismissible: true, // Modal can be dismissed by clicking outside of the modal
+    opacity: .5, // Opacity of modal background
+    in_duration: 300, // Transition in duration
+    out_duration: 200, // Transition out duration
+    starting_top: '4%', // Starting top style attribute
+    ending_top: '10%', // Ending top style attribute
+    ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+        $('#modal1').modal('close');
+        $('#modal3').modal('close');
+        console.log(modal, trigger);
+      }
+  }
+  );
+
+  $('#modal3').modal({
+    dismissible: true, // Modal can be dismissed by clicking outside of the modal
+    opacity: .5, // Opacity of modal background
+    in_duration: 300, // Transition in duration
+    out_duration: 200, // Transition out duration
+    starting_top: '4%', // Starting top style attribute
+    ending_top: '10%', // Ending top style attribute
+    ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+        $('#modal1').modal('close');
+        $('#modal2').modal('close');
+        console.log(modal, trigger);
+      }
+  }
+  );
+
+  $(".dropdown-button").dropdown();
+
+});
+
