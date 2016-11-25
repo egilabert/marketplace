1a5dc8030e6a5cc0f6fe7fa1083ac262b9bd7684
Enric Gilabert <gilabert.enric@gmail.com>
2016-11-25 17:03:42 +0100

    empresas template dir2

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'marketplace.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'templates/empresas')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'templates')+"/empresas"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
