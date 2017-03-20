5c2c7a6a4f1039744118f967e7f684bedcc41041
Enric Gilabert <gilabert.enric@gmail.com>
2017-03-20 18:22:53 +0100

    cro utf-8

--- risk_cro/views.py
+++ risk_cro/views.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
 from django.conf import settings
 from django.shortcuts import render, get_object_or_404
